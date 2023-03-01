012707f4929be9de568aab76ce56dd43fb8e2663
Abel Misiocha <wells.mail.13@gmail.com>
2023-03-02 02:54:17 +0300

    Delete nav bar directory

--- nav bar/script.js
+++
@@ -1,7 +0,0 @@
-const uList = document.querySelector("#listId");
-const btn = document.querySelector(".btn");
-
-const show = () => {
-  uList.classList.toggle("is-active");
-};
-btn.addEventListener("click", show);
